9b37f094122d92672e6c3d17a6d465b913c07ceb
Jesus Vargas <jesusvarguer18@gmail.com>
2022-09-28 16:17:25 -0500

    levelFail

--- game.js
+++ game.js
@@ -8,6 +8,7 @@ const btnDown = document.querySelector('#down');
 let canvasSize;
 let elementsSize;
 let level = 0;
+let lives = 3;
 
 const playerPosition = {
   x: undefined,
@@ -102,7 +103,7 @@ function movePlayer() {
   });
 
   if (enemyCollision) {
-    console.log('Chocaste contra un enemigo :(');
+    levelFail();
   }
 
   game.fillText(emojis['PLAYER'], playerPosition.x, playerPosition.y);
@@ -111,6 +112,21 @@ function movePlayer() {
 function levelWin() {
   console.log('Subiste de nivel');
   level++;
+  startGame();
+}
+
+function levelFail() {
+  console.log('Chocaste con un enemigo :(');
+  lives--;
+
+  console.log(lives);
+
+  if (lives <= 0) {
+    level = 0;
+  }
+
+  playerPosition.x = undefined;
+  playerPosition.y = undefined;
   startGame();
 }
 
