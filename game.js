3068a28160180db2cb8b8ad1fcff9792119e1715
Jesus Vargas <jesusvarguer18@gmail.com>
2022-09-28 14:56:40 -0500

    Enemies Collision

--- game.js
+++ game.js
@@ -17,6 +17,8 @@ const giftPosition = {
   y: undefined,
 };
 
+let enemyPositions = [];
+
 window.addEventListener('load', setCanvasSize);
 window.addEventListener('resize', setCanvasSize);
 
@@ -46,7 +48,10 @@ function startGame() {
   const mapRowCols = mapRows.map(row => row.trim().split(''));
   console.log({map, mapRows, mapRowCols});
 
+  enemyPositions = [];
   game.clearRect(0,0,canvasSize, canvasSize);
+
+
   mapRowCols.forEach((row, rowI) => {
     row.forEach((col, colI) => {
       const emoji = emojis[col];
@@ -62,6 +67,11 @@ function startGame() {
       } else if (col == 'I') {
         giftPosition.x = posX;
         giftPosition.y = posY;
+      } else if (col == 'X') {
+        enemyPositions.push({
+          x: posX,
+          y: posY,
+        });
       }
 
       game.fillText(emoji, posX, posY);
@@ -77,7 +87,17 @@ function movePlayer() {
   const giftCollision = giftCollisionX && giftCollisionY;
 
   if (giftCollision) {
-    console.log('Subiste de nisvel!');
+    console.log('Subiste de nivel!');
+  }
+
+  const enemyCollision = enemyPositions.find(enemy => {
+    const enemyCollisionX = enemy.x.toFixed(3) == playerPosition.x.toFixed(3);
+    const enemyCollisionY = enemy.y.toFixed(3) == playerPosition.y.toFixed(3);
+    return enemyCollisionX && enemyCollisionY;
+  });
+
+  if (enemyCollision) {
+    console.log('Chocaste contra un objeto!');
   }
 
   game.fillText(emojis['PLAYER'], playerPosition.x, playerPosition.y);
