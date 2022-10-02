9b9c104874cc561620ff3db3f339aeffcd17f0ec
Jesus Vargas <jesusvarguer18@gmail.com>
2022-10-02 16:26:31 -0500

    Record

--- game.js
+++ game.js
@@ -6,6 +6,9 @@ const btnRight = document.querySelector('#right');
 const btnDown = document.querySelector('#down');
 const spanLives = document.querySelector('#lives');
 const spanTime = document.querySelector('#time');
+const spanRecord = document.querySelector('#record');
+const pResult = document.querySelector('#result');
+
 
 let canvasSize;
 let elementsSize;
@@ -60,6 +63,7 @@ function startGame() {
   if (!timeStart) {
     timeStart = Date.now();
     timeInterval = setInterval(showTime, 100);
+    showRecord();
   }
 
   const mapRows = map.trim().split('\n');
@@ -146,6 +150,22 @@ function levelFail() {
 function gameWin() {
   console.log('¡Terminaste el juego!');
   clearInterval(timeInterval);
+
+  const recordTime = localStorage.getItem('record_time');
+  const playerTime = Date.now() - timeStart;
+  if (recordTime) {
+    if (recordTime >= playerTime) {
+      localStorage.setItem('record_time', playerTime);
+      pResult.innerHTML = 'Superaste el record :)';
+    } else {
+      pResult.innerHTML = 'Lo siento no superaste el record :(';
+    }
+  } else {
+    localStorage.setItem('record_time', playerTime);
+    pResult.innerHTML = 'Primera vez? Muy bien intenta superar tu tiempo :)';
+  }
+
+  console.log({recordTime, playerTime});
 }
 
 function showLives() {
@@ -157,6 +177,10 @@ function showLives() {
 
 function showTime() {
   spanTime.innerHTML = Date.now() - timeStart;
+}
+
+function showRecord() {
+  spanRecord.innerHTML =  localStorage.getItem('record_time')
 }
 
 window.addEventListener('keydown', moveByKeys);
